369aae1941998e8a0f3b40b89a50360657fd9009
Vanco <van.fan@outlook.com>
2025-04-19 15:10:53 +0800

    Update autobuild-package.sh

--- autobuild-package.sh
+++ autobuild-package.sh
@@ -5,7 +5,7 @@
 cd build/usr/src/broadcom-wl-6.30.223.271
 
 # Build the package
-make clean && make CC=clang LD=ld.lld
+make clean && make CC=clang LD=ld.lld --silent
 
 # Check if the build was successful
 if [ $? -ne 0 ]; then
@@ -25,7 +25,7 @@ cp -r usr/src/broadcom-wl-6.30.223.271/wl.ko usr/lib/modules/$(uname -r)/kernel/
 # Package the built files:
 # Tar the files in usr/lib usr/share, exclude the source code
 
-tar -I zstd -cf ../broadcom-wl.tar.zst --exclude=src -C usr .
+tar -I zstd -cf ../broadcom-wl-`uname -r`.tar.zst --exclude=src -C usr .
 # Check if the package was created successfully
 if [ $? -ne 0 ]; then
     echo "Failed to create the package."
